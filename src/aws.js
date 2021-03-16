b59ad33ad00a5b40ea4176ceeeca04dffbfa50db
Jordan Schidlowsky <jordanschidlowsky@gmail.com>
2021-03-15 23:36:16 -0600

    need home for doctl

--- src/aws.js
+++ src/aws.js
@@ -12,6 +12,8 @@ async function startEc2Instance(label, githubRegistrationToken) {
     'mkdir actions-runner && cd actions-runner',
     'curl -O -L https://github.com/actions/runner/releases/download/v2.277.1/actions-runner-linux-x64-2.277.1.tar.gz',
     'tar xzf ./actions-runner-linux-x64-2.275.1.tar.gz',
+    'export RUNNER_ALLOW_RUNASROOT=1',
+    'export HOME=/root'
     `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
     './run.sh',
   ];
